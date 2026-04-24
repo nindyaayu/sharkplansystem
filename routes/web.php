32badf9adb0a5efaa29a4ee773d8fd02faa1bf8b
nindyaayu <nindya.ayu5p@gmail.com>
2026-04-24 11:47:36 +0700

    start project, hubungkan ke database dan buat tampilan awal

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('login');
+});
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [AuthController::class, 'dashboard']);
+    Route::get('/logout', [AuthController::class, 'logout']);
 });
